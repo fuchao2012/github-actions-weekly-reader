3053b717ca2fb07cdeac273d4365a2c4f02792e5
fuchao2012 <zhengfuchao2008@126.com>
2020-01-06 19:49:42 +0800

    add fangtang

--- index.sh
+++ index.sh
@@ -9,3 +9,4 @@ DAY=`date +%d`
 
 curl -o result.html http://fex.baidu.com/blog/$YEAR/$MONTH/fex-weekly-$DAY/
 
+curl https://sc.ftqq.com/SCU76267Tf430e6fb74c4f23755174639a87116715e131c469d7cc.send\?text\="Weekly Reader"\ -F "file=result.html" -v
